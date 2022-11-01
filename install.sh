483709900c721e7df891f16f1526c53f067949f5
DN-debug <devojyoti95nandi@gmail.com>
2022-11-01 12:24:23 +0530

    script changes

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ pacman -S latte-dock
 
 # Move the files
 cp -r .config/* ~/.config/
-cp -r usr/* /usr/
+cp -r usr/share/* /usr/share/
+cp -r usr/sbin/uniform_cursor /usr/sbin/
+cp -r usr/local/bin/layoutChanger /usr/local/bin/
 
 # Make the scripts executable
 chmod +x /usr/local/bin/layoutChanger
@@ -19,4 +21,4 @@ chmod +x /usr/share/layouts/chrome
 chmod +x /usr/share/layouts/unity
 chmod +x /usr/sbin/uniform_cursor
 
-Exit
+echo "Done!"
